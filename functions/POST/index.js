1753c336a48bfb5999bcb76edeb69d526c2dd3f0
Marvan Al Kufai <alkufai@gmail.com>
2023-09-13 09:39:53 +0200

    added function to check roomtypes availability on specific date

--- functions/POST/index.js
+++ functions/POST/index.js
@@ -25,6 +25,26 @@ const roomType = [
     maxGuests: 3,
   },
 ];
+
+async function checkIfRoomTypeIsBooked(checkInDate, checkOutDate, roomTypeId) {
+  for (let i = 0; i < roomTypeId.length; i++) {
+    const params = {
+      TableName: 'Booking',
+      FilterExpression:
+        'contains(bookedRoomsId, :roomTypeId) AND ((checkInDate BETWEEN :checkInDate AND :checkOutDate) OR (checkOutDate BETWEEN :checkInDate AND :checkOutDate))',
+      ExpressionAttributeValues: {
+        ':checkInDate': checkInDate,
+        ':checkOutDate': checkOutDate,
+        ':roomTypeId': roomTypeId[i],
+      },
+    };
+
+    const result = await db.scan(params).promise();
+    if (result.Items.length > 0) return true;
+  }
+  return false;
+}
+
 function calculateNumberOfNights(checkInDate, checkOutDate) {
   const checkIn = new Date(checkInDate);
   const checkOut = new Date(checkOutDate);
@@ -86,7 +106,7 @@ exports.handler = async (event, context) => {
       checkInDate,
       checkOutDate,
     } = JSON.parse(event.body);
-    console.log('event.body', event.body);
+
     if (
       !guestName ||
       !numberOfGuests ||
@@ -100,6 +120,18 @@ exports.handler = async (event, context) => {
       });
     }
 
+    const bookingExists = await checkIfRoomTypeIsBooked(
+      checkInDate,
+      checkOutDate,
+      bookedRoomsId
+    );
+    if (bookingExists) {
+      return sendResponse(400, {
+        success: false,
+        message: 'Room type is already booked for provided dates',
+      });
+    }
+
     const savedBooking = await bookRoom(
       guestName,
       numberOfGuests,
@@ -108,7 +140,10 @@ exports.handler = async (event, context) => {
       checkOutDate
     );
 
-    return sendResponse(200, { success: true, booking: savedBooking });
+    return sendResponse(200, {
+      success: true,
+      booking: savedBooking,
+    });
   } catch (error) {
     console.log(error);
     return sendResponse(500, {
